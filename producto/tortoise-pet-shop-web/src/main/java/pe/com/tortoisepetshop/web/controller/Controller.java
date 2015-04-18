ba4ae23eaf1a5a950ccec3a8d4540fed7511fbe4
fcaceresg <fcaceresg@outlook.com>
2015-04-18 10:13:51 -0500

    Creación de la clase controladora para las rutas

---
+++ producto/tortoise-pet-shop-web/src/main/java/pe/com/tortoisepetshop/web/controller/Controller.java
@@ -0,0 +1,19 @@
+package pe.com.tortoisepetshop.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+
+public class Controller {
+    
+    public static void main(String[] args) {
+        get("/hello", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+            return new ModelAndView(attributes, "hello.ftl");
+        }, new FreeMarkerEngine());
+    }
+    
+}
