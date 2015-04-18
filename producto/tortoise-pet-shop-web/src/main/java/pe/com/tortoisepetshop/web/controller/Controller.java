50598a2725253a22bc7babd98728f1cd588326aa
fcaceresg <fcaceresg@outlook.com>
2015-04-18 14:06:08 -0500

    Eliminación de variables de prueba innecesarias.

--- producto/tortoise-pet-shop-web/src/main/java/pe/com/tortoisepetshop/web/controller/Controller.java
+++ producto/tortoise-pet-shop-web/src/main/java/pe/com/tortoisepetshop/web/controller/Controller.java
@@ -7,14 +7,12 @@ import static spark.Spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
 public class Controller {
-    int b = 0;
     
     public static void main(String[] args) {
         get("/hello", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
             return new ModelAndView(attributes, "hello.ftl");
-            int GGGG;
         }, new FreeMarkerEngine());
     }
     
