bf7205962445bd94700aed08a7cb0dcff822972d
fcaceresg <fcaceresg@outlook.com>
2015-04-27 17:19:04 -0500

    F

---
+++ tortoise-app/src/main/java/pe/com/tortoise/controller/Controller.java
@@ -0,0 +1,18 @@
+package pe.com.tortoise.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+
+public class Controller {
+    
+    public static void main(String[] args) {
+        get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            return new ModelAndView(attributes, "layout.ftl");
+        }, new FreeMarkerEngine());
+    }
+    
+}
