f96f482037d06b8a2e26e5f7ffd8d41b40642ff8
fcaceresg <fcaceresg@outlook.com>
2015-05-01 20:23:28 -0500

    Commit

--- tortoise-app/src/main/java/pe/com/tortoise/model/view/CategoriaView.java
+++ tortoise-app/src/main/java/pe/com/tortoise/model/view/CategoriaView.java
@@ -2,9 +2,11 @@
 package pe.com.tortoise.model.view;
 
 public class CategoriaView {
-    Integer identificador;
-    String descripcion;
-
-    public CategoriaView() {
+    public Integer identificador;
+    public String descripcion;
+    
+    public CategoriaView(Integer identificador, String descripcion) {
+        this.identificador = identificador;
+        this.descripcion = descripcion;
     }
 }
