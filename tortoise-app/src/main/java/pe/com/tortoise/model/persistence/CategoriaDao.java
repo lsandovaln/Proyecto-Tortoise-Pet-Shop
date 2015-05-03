623b04b140cf7be6314958c6b3a84cdef644ce8f
Franco Cáceres <fcaceresg@outlook.com>
2015-05-02 23:37:52 -0500

    Commit

--- tortoise-app/src/main/java/pe/com/tortoise/model/persistence/CategoriaDao.java
+++ tortoise-app/src/main/java/pe/com/tortoise/model/persistence/CategoriaDao.java
@@ -1,6 +1,192 @@
 
 package pe.com.tortoise.model.persistence;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pe.com.tortoise.model.business.Categoria;
+import pe.com.tortoise.model.persistence.util.Conexion;
+
 public class CategoriaDao {
+    public static synchronized boolean insertarCategoria(Categoria categoria) {
+        Connection cn = null;
+        Statement stmnt = null;
+        try {
+            cn = Conexion.obtenerConexion();
+            stmnt = cn.createStatement();
+            String sql = "INSERT INTO categoria(descripcion) VALUES(" +
+                    categoria.getDescripcion() + ")";
+            stmnt.executeUpdate(sql);
+            return true;
+        } catch(SQLException ex) {
+            return false;
+        } finally {
+            if(null != stmnt) {
+                try {
+                    stmnt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != cn) {
+                try {
+                    cn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
     
+    public static synchronized boolean actualizarCategoria(Categoria categoria) {
+        Connection cn = null;
+        Statement stmnt = null;
+        try {
+            cn = Conexion.obtenerConexion();
+            stmnt = cn.createStatement();
+            String sql = "UDATE categoria " +
+                    "SET descripcion=" + categoria.getDescripcion() +
+                    "WHERE id=" + categoria.getIdentificador().toString();
+            stmnt.executeUpdate(sql);
+            return true;
+        } catch(SQLException ex) {
+            return false;
+        } finally {
+            if(null != stmnt) {
+                try {
+                    stmnt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != cn) {
+                try {
+                    cn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    public static synchronized boolean eliminarCategoria(Integer id) {
+        Connection cn = null;
+        Statement stmnt = null;
+        try {
+            cn = Conexion.obtenerConexion();
+            stmnt = cn.createStatement();
+            String sql = "DELETE categoria"
+                    + "WHERE id=" + id.toString();
+            stmnt.executeUpdate(sql);
+            return true;
+        } catch(SQLException ex) {
+            return false;
+        } finally {
+            if(null != stmnt) {
+                try {
+                    stmnt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != cn) {
+                try {
+                    cn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    public static synchronized Categoria obtenerCategoria(Integer id) {
+        Connection cn = null;
+        Statement stmnt = null;
+        ResultSet rs = null;
+        try {
+            cn = Conexion.obtenerConexion();
+            stmnt = cn.createStatement();
+            String sql = "SELECT * FROM categoria WHERE id=" + id.toString();
+            rs = stmnt.executeQuery(sql);
+            Categoria categoria = new Categoria();
+            while(rs.next()) {
+                categoria.setIdentificador(rs.getInt("id"));
+                categoria.setDescripcion(rs.getString("descripcion"));
+            }
+            return categoria;
+        } catch(SQLException ex) {
+            return null;
+        } finally {
+            if(null != rs) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != stmnt) {
+                try {
+                    stmnt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != cn) {
+                try {
+                    cn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    public static synchronized List<Categoria> obtenerCategorias() {
+        Connection cn = null;
+        Statement stmnt = null;
+        ResultSet rs = null;
+        try {
+            cn = Conexion.obtenerConexion();
+            stmnt = cn.createStatement();
+            String sql = "SELECT * FROM categoria";
+            rs = stmnt.executeQuery(sql);
+            List<Categoria> categorias = new ArrayList<>();
+            while(rs.next()) {
+                Categoria categoria = new Categoria();
+                categoria.setIdentificador(rs.getInt("id"));
+                categoria.setDescripcion(rs.getString("descripcion"));
+                categorias.add(categoria);
+            }
+            return categorias;
+        } catch(SQLException ex) {
+            return null;
+        } finally {
+            if(null != rs) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != stmnt) {
+                try {
+                    stmnt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(null != cn) {
+                try {
+                    cn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(CategoriaDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
 }
--- tortoise-app/src/main/java/pe/com/tortoise/model/persistence/ProductoDao.java
+++ tortoise-app/src/main/java/pe/com/tortoise/model/persistence/ProductoDao.java
@@ -4,43 +4,41 @@ package pe.com.tortoise.model.persistence;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Types;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import pe.com.tortoise.model.business.Producto;
 import pe.com.tortoise.model.persistence.util.Conexion;
 
 public class ProductoDao {
     public static synchronized boolean insertarProducto(Producto producto) {
         Connection cn = null;
-        CallableStatement cl = null;
-        boolean rpta = false;
+        Statement stmnt = null;
         try {
-            String call = "{CALL spI_producto(?,?,?)}";
             cn = Conexion.obtenerConexion();
-            cn.setAutoCommit(false);
-            cl = cn.prepareCall(call);
-            cl.registerOutParameter(1, Types.INTEGER);
-            cl.setString(2, producto.getNombre());
-            cl.setDouble(3, producto.getPrecio());
-            rpta = cl.executeUpdate() == 1;
-            if (rpta) {
-                cn.commit();
-            } else {
-                Conexion.deshacerCambios(cn);
+            stmnt = cn.createStatement();
+            String sql = "";
+            stmnt.executeUpdate(sql);
+            return true;
+        } catch(SQLException ex) {
+            return false;
+        } finally {
+            if(null != stmnt) {
+                try {
+                    stmnt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ProductoDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-            Conexion.cerrarCall(cl);
-            Conexion.cerrarConexion(cn);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Conexion.deshacerCambios(cn);
-            Conexion.cerrarCall(cl);
-            Conexion.cerrarConexion(cn);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Conexion.deshacerCambios(cn);
-            Conexion.cerrarCall(cl);
-            Conexion.cerrarConexion(cn);
+            if(null != cn) {
+                try {
+                    cn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ProductoDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
-        return rpta;
     }
     
     public static synchronized boolean actualizarProducto(Producto producto) {
