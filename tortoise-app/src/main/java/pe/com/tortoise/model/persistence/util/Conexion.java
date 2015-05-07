c2b0fca5de6e0c2d5ab92df7f1689cdd3d9842fb
fcaceresg <fcaceresg@outlook.com>
2015-05-06 22:44:36 -0500

    Commit

--- tortoise-app/src/main/java/pe/com/tortoise/model/persistence/util/Conexion.java
+++ tortoise-app/src/main/java/pe/com/tortoise/model/persistence/util/Conexion.java
@@ -20,20 +20,4 @@ public class Conexion {
             return cn;
         }
     }
-    
-    public static synchronized void cerrarCall(CallableStatement cl) {
-        try{cl.close();}catch(Exception e){}
-    }
-    
-    public static synchronized void cerrarConexion(ResultSet rs) {
-        try{rs.close();} catch (Exception e) {}
-    }
-    
-    public static synchronized void cerrarConexion(Connection cn) {
-        try{cn.close();} catch (Exception e) {}
-    }
-    
-    public static synchronized void deshacerCambios(Connection cn) {
-        try{cn.rollback();}catch (Exception e){}
-    }
 }
